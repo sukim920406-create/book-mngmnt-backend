eb97429ad66260c7dd23c415b5f2f888d2028282
seongukim <csc9211@naver.com>
2026-06-11 13:15:12 +0900

    SearchLog Controller, Service 작성, Repository에 findBySearchLogId 추가(h2와 postman으로 test 완료)

--- src/main/java/com/aivle/bookapp/repository/SearchResultClickRepository.java
+++ src/main/java/com/aivle/bookapp/repository/SearchResultClickRepository.java
@@ -3,5 +3,8 @@ package com.aivle.bookapp.repository;
 import com.aivle.bookapp.domain.SearchResultClick;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SearchResultClickRepository extends JpaRepository<SearchResultClick, Long>{
+    List<SearchResultClick> findBySearchLogId(Long id);
 }
